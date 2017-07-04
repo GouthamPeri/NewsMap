2af3d7275ce210998b353d3550a2bc95657551af
SivaGoutham <sivagoutham369@gmail.com>
2017-07-04 19:14:29 +0530

    added a welcome page

--- newsmap/newsmap/settings.py
+++ newsmap/newsmap/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'newsmap_app.apps.NewsmapAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
