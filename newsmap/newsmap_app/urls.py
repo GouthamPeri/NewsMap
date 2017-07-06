dfc585a1e64c6db78bc665f04f1c4e18e6931cad
svsrk praveen <sk.sk.praveen@gmail.com>
2017-07-06 18:58:00 +0530

    Added map

--- newsmap/newsmap_app/urls.py
+++ newsmap/newsmap_app/urls.py
@@ -1,6 +1,9 @@
+from django.views.generic import TemplateView
+
 from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^home', views.welcome),
+    url(r'^home$', views.welcome),
+    url(r'^$', TemplateView.as_view(template_name="index.html"))
 ]
