726b52822527c481faee239b8b997be18a5cea19
SivaGoutham <sivagoutham369@gmail.com>
2017-07-08 12:21:01 +0530

    added markers

--- newsmap/newsmap_app/views.py
+++ newsmap/newsmap_app/views.py
@@ -26,6 +26,7 @@ city_urls = {'Mumbai' : -2128838597,'Delhi' : -2128839596, 'Banglore' : -2128833
 }
 
 
+
 def extract_feed(city_name):
     url = "http://timesofindia.indiatimes.com/rssfeeds/" + str(city_urls[city_name]) + ".cms"
     feed = feedparser.parse(url)
@@ -60,7 +61,24 @@ class RetrieveNewsItemsJSON(ListAPIView):
 
 
 def get_city_coords(request):
-    city_coords = {}
+    city_coords = {'Mumbai': {'lat': 19.0760, 'lng': 72.8777}, 'Delhi': {'lat': 28.7041, 'lng': 77.1025},
+                   'Banglore': {'lat': 12.9716, 'lng': 77.5946}, 'Hyderabad': {'lat': 17.3850, 'lng': 78.4867},
+                   'Chennai': {'lat': 13.0827, 'lng': 80.2707}, 'Ahemdabad': {'lat': 23.0225, 'lng': 72.5714},
+                   'Allahabad': {'lat': 25.4358, 'lng': 81.8463}, 'Bhubaneswar': {'lat': 20.2961, 'lng': 85.8245},
+                   'Coimbatore': {'lat': 11.0168, 'lng': 76.9558}, 'Gurgaon': {'lat': 28.4595, 'lng': 77.0266},
+                   'Guwahati': {'lat': 26.1445, 'lng': 91.7362}, 'Hubli': {'lat': 15.3647, 'lng': 75.1240},
+                   'Kanpur': {'lat': 26.4499, 'lng': 80.3319}, 'Kolkata': {'lat': 22.5726, 'lng': 88.3639},
+                   'Ludhiana': {'lat': 30.9010, 'lng': 75.8573}, 'Mangalore': {'lat': 12.9141, 'lng': 74.8560},
+                   'Mysore': {'lat': 12.2958, 'lng': 76.6394}, 'Noida': {'lat': 28.5355, 'lng': 77.3910},
+                   'Pune': {'lat': 18.5204, 'lng': 73.8567}, 'Goa': {'lat': 15.2993, 'lng': 74.1240},
+                   'Goa': {'lat': 15.2993, 'lng': 74.1240}, 'Chandigarh': {'lat': 30.7333, 'lng': 76.7794},
+                   'Lucknow': {'lat': 26.8467, 'lng': 80.9462}, 'Patna': {'lat': 25.5941, 'lng': 85.1376},
+                   'Jaipur': {'lat': 26.9124, 'lng': 75.7873}, 'Nagpur': {'lat': 21.1458, 'lng': 79.0882},
+                   'Rajkot': {'lat': 22.3039, 'lng': 70.8022}, 'Surat': {'lat': 21.1702, 'lng': 72.8311},
+                   'Vadodara': {'lat': 22.3072, 'lng': 73.1812}, 'Varanasi': {'lat': 25.3176, 'lng': 82.9739},
+                   'Thane': {'lat': 19.2183, 'lng': 72.9781}, 'Thiruvananthapuram': {'lat': 8.5241, 'lng': 76.9366}
+                  }
+
     return JsonResponse(json.dumps(city_coords), safe=False)
 
 
